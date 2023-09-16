cfaf14a78904e92428acf7b08be6d480b88f4260
donatto22 <ottanod22@gmail.com>
2023-09-16 10:49:06 -0500

    se crean los archivos y se crea el modelo estudiante

---
+++ src/main/java/com/project/backend/models/Student.java
@@ -0,0 +1,24 @@
+package com.project.backend.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data @Entity @Table(name = "student")
+public class Student {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long studentId;
+
+    @Column(length = 50)
+    private String firstname;
+
+    @Column(length = 50)
+    private String lastname;
+
+    @Column(unique = true)
+    private String email;
+}
